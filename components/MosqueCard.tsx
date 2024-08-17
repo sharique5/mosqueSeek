b6b1ce832cf4abc86b63c62e11f8036aaf460d70
Mohammed Sharique <msharique@microsoft.com>
2024-08-17 16:32:35 +0530

    navigation added

--- components/MosqueCard.tsx
+++ components/MosqueCard.tsx
@@ -1,11 +1,13 @@
 import React, {useState, useEffect} from 'react';
-import { View, Image, Text } from 'react-native';
+import { View, Image, Text, Platform, Alert } from 'react-native';
+import * as Linking from 'expo-linking';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import { commonStyles } from '@/stylesheets/common';
 import { mosqueCardStyles } from '@/stylesheets/mosqueCard';
 import { IMosqueCard } from '@/interfaces/mosqueLists';
 import { ENDPOINTS } from '@/constants/Common';
 import { getDistanceBetweenCoordinates } from '@/utility/dataService';
+import { formatString } from '@/utility/strings';
 
 const MosqueCard = (props: IMosqueCard) => {
   const [distance, setDistance] = useState<string>("");
@@ -18,6 +20,18 @@ const MosqueCard = (props: IMosqueCard) => {
         setDuration(res.duration);
       })
   }, [])
+
+  const openMaps = () => {
+    const url = Platform.select({
+      ios: `maps:?q=${props?.plusCode.globalCode}`, // Apple Maps
+      android: `geo:0,0?q=${props?.plusCode.globalCode}`, // Google Maps
+      default: formatString(ENDPOINTS.navigationQuery, { plusCode: props?.plusCode.globalCode }), // Web fallback
+    });
+
+    Linking.openURL(url).catch(() => {
+      Alert.alert('Error', 'Unable to open maps');
+    });
+  };
 
   return (
     <View style={[commonStyles.flexCenter, mosqueCardStyles.container]}>
@@ -35,7 +49,7 @@ const MosqueCard = (props: IMosqueCard) => {
           <Text style={mosqueCardStyles.distance}>{distance}, {duration}</Text>
         </View>
         <View style={[mosqueCardStyles.navigate, commonStyles.flexCenter]}>
-          <Ionicons name="navigate-circle-outline" size={48} color="blue" />
+          <Ionicons name="navigate-circle-outline" size={48} color="blue" onPress={openMaps} />
         </View>
       </View>
     </View>
