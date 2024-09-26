0ba9a0ccb3716e69086e3ef1d184c6794023a55b
Mohammed Sharique <msharique@microsoft.com>
2024-09-26 17:05:32 +0530

    update debug stmt

--- components/Welcome.tsx
+++ components/Welcome.tsx
@@ -9,7 +9,7 @@ import { welcomeStyles } from '@/stylesheets/welcome';
 import { Colors } from '@/constants/Colors';
 import Constants from 'expo-constants';
 
-const MAP_API_KEY = Constants.expoConfig?.extra?.MAP_API_KEY || "lets-see-this-time";
+const MAP_API_KEY = Constants.expoConfig?.extra?.MAP_API_KEY || "koi-humdum-na-raha";
 
 const Welcome = () => {
   const fadeAnim = useRef(new Animated.Value(0)).current; // Initial value for opacity: 0
